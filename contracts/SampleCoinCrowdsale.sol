ec2ab5b5de27333739c9a45c7acb6072a2530781
robot <tyler@tylerwells.net>
2018-01-07 13:05:49 +0800

    Fix CappedCrowdsale method

--- contracts/SampleCoinCrowdsale.sol
+++ contracts/SampleCoinCrowdsale.sol
@@ -6,14 +6,15 @@ pragma solidity ^0.4.13;
 import './SampleCoin.sol';
 import 'zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
 
-contract SampleCoinCrowdSale is CappedCrowdSale {
+contract SampleCoinCrowdSale is CappedCrowdsale {
 
   // CrowdSale.sol requires the following parameters:
   // startTime, endTime, rate (of eth to SampleCoin), wallet (of contract owner)
   // CappedCrowdSale additionally requires a cap parameter
 
   function SampleCoinCrowdSale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet, uint256 _cap) 
-    Crowdsale(_startTime, _endTime, _rate, _wallet, _cap)
+    Crowdsale(_startTime, _endTime, _rate, _wallet)
+    CappedCrowdsale(_cap)
     {}
 
   // createTokenContract creates the tokens to be sold
