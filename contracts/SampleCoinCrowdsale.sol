0ae7a87757fbcb18421434818aa1d729e5d55670
robot <tyler@tylerwells.net>
2018-01-07 12:43:18 +0800

    Add sample capped crowdsale contract

---
+++ contracts/SampleCoinCrowdsale.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.13;
+
+// We import our created SampleCoin as well as OpenZeppelin's 
+// CappedCrowdSale contract, which we extend and modify.
+
+import './SampleCoin.sol';
+import 'zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
+
+contract SampleCoinCrowdSale is CappedCrowdSale {
+
+  // CrowdSale.sol requires the following parameters:
+  // startTime, endTime, rate (of eth to SampleCoin), wallet (of contract owner)
+  // CappedCrowdSale additionally requires a cap parameter
+
+  function SampleCoinCrowdSale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet, uint256 _cap) 
+    Crowdsale(_startTime, _endTime, _rate, _wallet, _cap)
+    {}
+
+  // createTokenContract creates the tokens to be sold
+  // We override this method to have crowdsale of a specific MintableToken token
+  // named SampleCoin
+
+  function createTokenContract() internal returns (MintableToken) {
+    return new SampleCoin();
+  }
+
+}
